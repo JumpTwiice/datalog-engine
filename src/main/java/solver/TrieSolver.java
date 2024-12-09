2c48caa5413bfc232d3925e0e7459550c07585a0
Casper Dalgaard Nielsen <casper@dalgaardnielsen.dk>
2024-12-09 13:36:07 +0100

    Cleanup and renaming

--- src/main/java/solver/TrieSolver.java
+++ src/main/java/solver/TrieSolver.java
@@ -17,10 +17,8 @@ public class TrieSolver implements Solver<SimpleTrie> {
     }
 
     public TrieMap naiveEval() {
-//        TrieMap solutions = new TrieMap(p);
         boolean done;
         do {
-//            System.out.println("ITERATING");
             TrieMap newSolutions = new TrieMap(p);
             for (var p_i : p.rules.keySet()) {
                 newSolutions.put(p_i, eval(p_i, solutions));
@@ -37,14 +35,11 @@ public class TrieSolver implements Solver<SimpleTrie> {
     }
 
     public TrieMap semiNaiveEval() {
-//        Map<Long, Set<List<Long>>> solutions = initializeSolutions(p);
-//        TrieMap solutions = new TrieMap(p);
         TrieMap deltaSolutions = new TrieMap(p);
         for (var p_i : p.rules.keySet()) {
             deltaSolutions.put(p_i, eval(p_i, solutions));
         }
         solutions.meld(deltaSolutions.cloneTrieSet());
-//        solutions = deltaSolutions.cloneTrieSet();
 
         boolean done;
         do {
@@ -80,7 +75,6 @@ public class TrieSolver implements Solver<SimpleTrie> {
     }
 
     public SimpleTrie eval(long p_i, TrieMap solutions) {
-//        var sol = new SimpleTrie(p.rules.get(p_i).size());
         var sol = new SimpleTrie(-1);
         for (var r : p.rules.get(p_i)) {
             sol.meld(evalRule(r, r.body.stream().map(x -> solutions.get(x.pred)).toList()));
@@ -103,31 +97,12 @@ public class TrieSolver implements Solver<SimpleTrie> {
         if (relations.stream().anyMatch(Objects::isNull)) {
             return null;
         }
-//        System.out.println("JOINING");
-//        System.out.println(relations);
-//        System.out.println(r.toString(p));
         var join = join(r, relations);
-//        System.out.println("JOIN");
-//        System.out.println(join);
         if (join == null) {
             return null;
         }
-//        System.out.println("PROJECTION");
-//        System.out.println(join.projectTo(r));
         return join.projectTo(r);
     }
-
-
-//    public SimpleTrie evalRule(Rule r, TrieMap solutions) {
-//        if(r.body.stream().anyMatch(x -> solutions.get(x.pred) == null)) {
-//            return null;
-//        }
-//        var join = join(r, solutions);
-//        if (join == null) {
-//            return null;
-//        }
-//        return join.projectTo(r);
-//    }
 
     public SimpleTrie evalRuleIncremental(Rule r, TrieMap solutions, TrieMap newSolutions) {
         SimpleTrie result = null;
@@ -135,28 +110,11 @@ public class TrieSolver implements Solver<SimpleTrie> {
         for (var i = 0; i < sols.size(); i++) {
             var atomID = r.body.get(i).pred;
             sols.set(i, newSolutions.get(atomID));
-//            solutions.put(x, newSolutions.get(x));
-//            var res = evalRule(r, sols);
             result = solutions.meldSimpleTries(result, evalRule(r, sols));
             sols.set(i, solutions.get(atomID));
         }
-//        var result = meaningFullRelations.stream().map(x -> {
-////        var result = p.rules.keySet().stream().map(x -> {
-////        var result = solutions.keySet().stream().map(x -> {
-////            TODO: If we want to do it in parallel we need to make a shallow clone of the map.
-//            var temp = sols.get(x);
-//            solutions.put(x, newSolutions.get(x));
-//            var res = evalRule(r, solutions);
-//            solutions.put(x, temp);
-//            return res;
-//        }).reduce(null, solutions::meldSimpleTries);
-
         return result;
     }
-
-//    public SimpleTrie join(Rule r, List<SimpleTrie> solutions) {
-//        return generateConstraints(r, solutions);
-//    }
 
     /**
      * Iteratively builds the trie for the rule.
@@ -174,7 +132,7 @@ public class TrieSolver implements Solver<SimpleTrie> {
         if (source == null) {
             return null;
         }
-        var prev = TrieMap.cloneForTrie(source, constBool, constArr, sameArr);
+        var prev = TrieMap.query(source, constBool, constArr, sameArr);
         for (var i = 1; i < solutions.size(); i++) {
             prev = TrieMap.combine(prev, r.body.get(i), solutions.get(i), r);
             if (prev == null) {
